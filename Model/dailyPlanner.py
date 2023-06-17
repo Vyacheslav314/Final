794f7788bf81b183e3d8e73bc8a684d6132d736f
Vyacheslav <vyacheslav.trubaev@mail.ru>
2023-06-17 14:07:01 +0300

    start

---
+++ Model/dailyPlanner.py
@@ -0,0 +1,11 @@
+import uuid
+
+class DaillyPlanner:
+    
+    
+    def __init__(self, id = 0, date = "", time = "", coment = ""):
+        self.id = id
+        self.date = date
+        self.time = time
+        self.coment = coment
+
